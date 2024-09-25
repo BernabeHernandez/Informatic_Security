a5e67dd6ed58c92d360bfc3817c392cac608f529
Bernabe <20221130@uthh.edu.mx>
2024-09-24 19:34:16 -0600

    Primer commit

--- src/componentes/Compartidos/PieDePaginaAdmin.jsx
+++ src/componentes/Compartidos/PieDePaginaAdmin.jsx
@@ -41,26 +41,54 @@ const PieDePaginaCliente = () => {
       });
   }, []);
 
-
   return (
-<Layout>
-    <Footer style={{ 
-        backgroundColor: '#0979B0', 
-        textAlign: 'center', 
-        padding: '40px 20px', 
-        maxWidth: '100%', 
-        boxSizing: 'border-box', 
-        display: 'grid', 
-        gridTemplateColumns: '1fr 1fr 1fr',
-        '@media (max-width: 600px)': {
+    <Layout>
+      <Footer
+        style={{
+          backgroundColor: '#0979B0',
+          textAlign: 'center',
+          padding: '40px 20px',
+          maxWidth: '100%',
+          boxSizing: 'border-box',
+          display: 'grid',
+          gridTemplateColumns: '1fr 1fr 1fr',
+          '@media (max-width: 600px)': {
             gridTemplateColumns: '1fr',
             padding: '20px 10px',
-        },
-    }}>
+          },
+        }}
+      >
 
-    </Footer>
-</Layout>
-
+        <div>
+          <Menu mode="horizontal" style={{ backgroundColor: '#0979B0', borderBottom: 'none' }}>
+            {datosEmpresa.redesSociales.facebook && (
+              <Menu.Item key="facebook">
+                <Link to={datosEmpresa.redesSociales.facebook}>
+                  <FacebookOutlined style={{ fontSize: '24px', color: '#fff' }} />
+                </Link>
+              </Menu.Item>
+            )}
+            {datosEmpresa.redesSociales.twitter && (
+              <Menu.Item key="twitter">
+                <Link to={datosEmpresa.redesSociales.twitter}>
+                  <TwitterOutlined style={{ fontSize: '24px', color: '#fff' }} />
+                </Link>
+              </Menu.Item>
+            )}
+            {datosEmpresa.redesSociales.instagram && (
+              <Menu.Item key="instagram">
+                <Link to={datosEmpresa.redesSociales.instagram}>
+                  <InstagramOutlined style={{ fontSize: '24px', color: '#fff' }} />
+                </Link>
+              </Menu.Item>
+            )}
+          </Menu>
+        </div>  
+        <div>
+          <Text style={{ color: '#fff' }}>© {new Date().getFullYear()} Bernabe HB. Todos los derechos reservados.</Text>
+        </div>
+      </Footer>
+    </Layout>
   );
 };
 
